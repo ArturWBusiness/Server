73ba452fdc79af9756686fb0ea2b1650ff830c01
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2018-01-30 18:16:24 +0000

    TASK UPDATE
    I went ahead and done task 4 and 5. Though task 5 took 2 min to generate.

--- Puzzle solving/README.txt
+++ Puzzle solving/README.txt
@@ -1,3 +1,9 @@
 Tasks are taken from projecteuler.net
 To get to the task sire input:
 https://projecteuler.net/problem={TaskNumber}
+
+Notes:
+
+Task 3: not solved
+
+Task 5: takes too long to generate answers above 16 (20 takes 2min)
--- Puzzle solving/Task 3.py
+++ Puzzle solving/Task 3.py
@@ -5,3 +5,13 @@ What is the largest prime factor of the number 600851475143 ?
 """
 # Try 1
 # no idea
+number = int(input("Number:"))
+numbers = []
+i = 1
+while True:
+    print(str(i))
+    i += i
+    if i > number:
+        break
+
+print("Largest factor is" + str(numbers[len(numbers)-1]))
