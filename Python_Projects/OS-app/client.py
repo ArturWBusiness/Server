ce323de7c0640a285a71d51d3112da312b9fb79e
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2018-03-27 20:18:44 +0100

    UPDATE

--- Python_Projects/OS-app/client.py
+++ Python_Projects/OS-app/client.py
@@ -1,1 +1,16 @@
 import _thread as thread
+import socket
+import tkinter
+
+
+algorithm:
+if no config
+    make it with defualt options
+else
+    read config.txt
+    update the version
+    set up a version
+
+load graphics
+
+set up defualt connection
