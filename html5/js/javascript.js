71985ddec79d991536b0d0241042c92ef5ddfb8d
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2019-04-07 12:44:09 +0100

    typescript learning now

--- html5/js/javascript.js
+++ html5/js/javascript.js
@@ -4,7 +4,7 @@ function startGame() {
 	myGameArea.start();
 	myGamePiece = new component(30, 30, "red", 10, 120);
 }
-
+let t = 0;
 function component(width, height, color, x, y) {
 	this.width = width;
 	this.height = height;
@@ -12,17 +12,21 @@ function component(width, height, color, x, y) {
 	this.y = y;
 	this.color = color;
 	this.update = function() {
+		let i = t/20;
+		this.x = Math.cos(Math.random());
+		this.y = Math.cos(i);
 		ctx = myGameArea.context;
 		ctx.fillStyle = this.color;
-		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.fillRect(this.x*100 + 100, this.y*100 + 100, 5, 5);
+		t++;
 	}
 }
 
 var myGameArea = {
     canvas: document.getElementById("canvas"),
     start: function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 300;
+        this.canvas.height = 300;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 16)
@@ -33,7 +37,7 @@ var myGameArea = {
 }
 
 function updateGameArea() {
-	myGameArea.clear();
+	//myGameArea.clear();
 	myGamePiece.update();
 }
 
